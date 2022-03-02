d31b74957b1375f8cceb2e8ba238dad26d744985
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-03-01 21:08:02 -0500

    commit tree 54bf698471ab771bccdbe654fe02e8d8dc278e55

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+const leastFavoriteCustomer = "yomama"
+
+function upperCaseCustomerName() {
+    var up = customerName.toUpperCase()
+    return up
+}
+   
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    var bestCustomer = 'jimothy'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+   let leastFavoriteCustomer = "who"
+}
